8076ee8be13a0353416c8931c3344673fbb68a5b
shell <shellljx@gmail.com>
2015-11-12 10:21:51 +0800

    小修改

---
+++ app/src/main/java/licrafter/com/v2ex/activity/TopicActivity.java
@@ -0,0 +1,14 @@
+package licrafter.com.v2ex.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by shell on 15-11-12.
+ */
+public class TopicActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
